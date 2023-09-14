7a17ab30d653c7d2d54925eca424550cb053b195
hyrondelle <n.hyr@free.fr>
2023-09-14 15:36:10 +0200

    config firebase

---
+++ src/firebase.config.tsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getAuth,GoogleAuthProvider} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING,
+  appId: import.meta.env.VITE_APP_APPID
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
