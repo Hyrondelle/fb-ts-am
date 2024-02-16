f392bd94a3d25cf5fddccc9b27b4b466357c9262
hyrondelle <n.hyr@free.fr>
2024-02-16 15:59:13 +0100

    uid

--- src/actions/user.actions.tsx
+++ src/actions/user.actions.tsx
@@ -1,20 +1,21 @@
 //import axios from "axios";
+
 import { useDispatch } from "react-redux";
 import { getAuth} from "firebase/auth";
 
 export const GET_USER = "GET_USER";
 
 
-
 export const GetUser = (uid:any) =>{
-    const dispatch:any = useDispatch()
-    const auth = getAuth();
-    return (dispatch) =>{
+    const dispatch:any = useDispatch();
+    const auth:any = getAuth();
+    return (dispatch:any) =>{
         return getAuth()
             .getUser(uid)
             .then((userRecord:any) => {
             // See the UserRecord reference doc for the contents of userRecord.
             console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+            dispatch({type:GET_USER,payload:userRecord.data})
             })
             .catch((error:any) => {
             console.log('Error fetching user data:', error);
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,7 @@
 //import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { db,auth } from '../firebase.config';
+//import { db,auth } from '../firebase.config';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
