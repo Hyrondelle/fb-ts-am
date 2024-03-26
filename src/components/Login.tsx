26109072e61e21a05a06d035e33447104426346d
hyrondelle <n.hyr@free.fr>
2024-03-26 11:17:54 +0100

    login error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 //import axios from 'axios';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 //import { db,auth } from '../firebase.config';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
@@ -9,24 +9,38 @@ const Login = () => {
     const [email,setEmail] = useState<string>('');
     const [password,setPassword] = useState<string>('');
     const navigate = useNavigate();
+    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
     const emailError:HTMLElement =document.querySelector('.emailError') as HTMLElement;
     const passwordError:HTMLElement =document.querySelector('.passwordError') as HTMLElement;
 
     const SubmitLogin = async(e:any) =>{
         e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-         // Signed in 
-        const user = userCredential.user;
-        console.log(user)
-        localStorage.setItem('userId',user.uid)
-        navigate("/home")
-        // ...
-        })
-        .catch((error) => {
-         const errorCode = error.code;
-        const errorMessage = error.message;
-        });
+        if(!emailRegex.test(email)){
+            emailError.innerHTML='email incorrect';
+        }
+        else{
+            emailError.innerHTML='';
+            if(password.length < 6){
+                passwordError.innerHTML='6 caractères min';
+            }
+            else{
+                passwordError.innerHTML='';
+                signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user)
+                localStorage.setItem('userId',user.uid)
+                navigate("/home")
+                // ...
+                })
+                .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                });
+            }
+        }
+        
     }
     return (
         
