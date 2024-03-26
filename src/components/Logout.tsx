1fe84124a2814f94de2fd120484da7c118db1dec
hyrondelle <n.hyr@free.fr>
2024-03-26 11:46:50 +0100

    maj logout

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
+    const auth = getAuth();
+    const navigate = useNavigate();
+    signOut(auth).then(() => {
+    // Sign-out successful.
+    navigate('/')
+    }).catch((error) => {
+    // An error happened.
+    console.log(error);
+    });
+
     return (
         <div>
             
