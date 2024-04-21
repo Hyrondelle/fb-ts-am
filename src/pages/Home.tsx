69863d35f882e147472b1f57273b345f1d3d421d
Hyrondelle <n.hyr@free.fr>
2024-04-22 00:54:05 +0200

    photo home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,30 @@
 import { useEffect, useState} from 'react';
 import Post from '../components/Post';
 import { getDocs, collection, doc ,setDoc, getDoc } from 'firebase/firestore';
+import { getStorage, ref,uploadBytes,getDownloadURL } from "firebase/storage";
 import { db } from '../firebase.config';
 import Nav from '../components/Nav';
+import { getUser } from '../Store';
 
 const Home = () => {
     const [post,setPost] = useState<string>('');
     const [listPost,setListPost] = useState<Array<any>>([]);
+    const [photo,setPhoto] = useState<any>({});
     const postCollectionRef = collection(db,"posts");
     const idUser:any = localStorage.getItem('userId');
+    const storage = getStorage();
+    const {id}:any = getUser();
+    
+    const sendPhoto = (e:any) =>{
+        e.preventDefault()
+        const imagesRef = ref(storage, 'photos/'+id+'/'+id+'.jpg');
+        uploadBytes(imagesRef, photo)
+        .then(() => {
+          console.log('file Uploaded!');
+        })
+        .catch((e)=>console.log('pb upload'+e)
+        );
+    }
 
     const sendPost = async (e:any) => {
         e.preventDefault();
@@ -52,21 +68,50 @@ const Home = () => {
         }
         getPostList();
     },[listPost, postCollectionRef])
+
+    const handleChange =async 
+    (e:any) =>{
+        setPhoto(e.target.files[0])
+        const imagesRef = ref(storage, 'photos/'+id+'/'+id+'.jpg');
+        uploadBytes(imagesRef, photo)
+        .then(() => {
+          console.log('file Uploaded!');
+        })
+        .catch((e)=>console.log('pb upload'+e)
+        );
+        try{
+            await getDownloadURL(ref(storage, 'photos/'+id+'/'+id+'.jpg'))
+                .then((url) => {
+                  const img = document.getElementById('photo');
+                  img?.setAttribute('src', url);
+                })
+                .catch((err) =>console.log(err)
+                )
+          }
+          catch(err){
+            console.log(err);
+          }
+      }
     
     return (
         <div className='home'>
             <Nav/>
             <form className='newMessage' onSubmit={sendPost}>
-                <label className='form-lab' htmlFor="post">nouveau message:</label>
-                <textarea className='form-text'
-                    autoFocus onChange={(e)=>setPost(e.target.value)}  
-                    name="post" 
-                    id="post" 
-                    value={post}/>
+                <label className='form-lab' htmlFor="post">nouveau message:
+                <div className='photo-message'>
+                    <img id='photo' src=''></img>
+
+                    <textarea className='form-text'
+                        autoFocus onChange={(e)=>setPost(e.target.value)}  
+                        name="post" 
+                        id="post" 
+                        value={post[0]}/>
+                </div>
+                </label>
                 <div className='photo-envoi'>
                     <label htmlFor="addPhoto">
                     <div className='form-btn file'>photo</div>
-                    <input type="file" name="addPhoto" id="addPhoto" />
+                    <input onChange={handleChange} type="file" name="addPhoto" id="addPhoto" />
                     </label>
                     <button className='form-btn' type="submit">Envoyer</button>
                 </div>
