af4069d504e4bfe91f325d126f6b6267c1972745
Hyrondelle <n.hyr@free.fr>
2024-05-20 11:49:24 +0200

    other style

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Friends = () => {
     return (
         <div className='friends'>
+            <h2>Vos amis</h2>
             <ul>
                 <li><h3>Victor Ramirez</h3><img src="src\assets\images4.jpeg" alt="prophil picture" /></li>
                 <li><h3>Isavelle Moreau</h3><img src="src\assets\images5.jpeg" alt="prophil picture" /></li>
--- src/components/OtherPeople.tsx
+++ src/components/OtherPeople.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 const OtherPeople = () => {
     return (
         <div className='otherPeople'>
-            
+            <h2>Connaissez-vous?</h2>
+            <ul>
+                <li><h3>Arthur Kobalski</h3><img src="src\assets\imagesprophil.jpeg" alt="prophil picture" /></li>
+                <li><h3>Esteban Sanchez</h3><img src="src\assets\photo3.jpg" alt="prophil picture" /></li>
+                <li><h3>Bernard Sauzer</h3><img src="src\assets\profile-picture.jpeg" alt="prophil picture" /></li>
+                <li><h3>Elise Giseau</h3><img src="src\assets\prophil2.jpg" alt="prophil picture" /></li>
+                <li><h3>Sophia Jerbalie</h3><img src="src\assets\prophil5.jpg" alt="prophil picture" /></li>
+                <li><h3>Arnaut Tibalu</h3><img src="src\assets\prophil7.jpg" alt="prophil picture" /></li>
+            </ul>
         </div>
     );
 };
