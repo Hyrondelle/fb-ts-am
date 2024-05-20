6f4342ba1547f94a5c64b10fa2cd5d2e93738260
Hyrondelle <n.hyr@free.fr>
2024-05-20 12:02:08 +0200

    add picture prophil on nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,32 @@
 import {Link} from 'react-router-dom'
 import {ImExit} from 'react-icons/im'
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { getUser } from '../Store';
 import Logout from './Logout';
 
 const Nav = () => {
+  const storage = getStorage(); 
+  const {pseudo,id}:any = getUser();
+  
+  try{
+    getDownloadURL(ref(storage, 'profil/'+id+'/'+id+'.jpg'))
+    .then((url) => {
+    const img = document.getElementById('profilNav');
+    img?.setAttribute('src', url);
+    })
+    .catch((err) =>console.log(err))
+  }
+  catch(err){
+    console.log(err);
+  }
 
-  const {pseudo}:any = getUser();
     return (
         <nav>
           {(window.location.pathname=='/home')?<Link aria-label='go profil page' to="/profil">profil</Link>:<Link to="/home">home</Link>}
           {(pseudo?
           <div className="pseudo-exit">
-            <h1>Bonjour {pseudo}</h1>
+            <img id='profilNav' src='src\assets\defautProfil.png' alt='profil picture'></img>
+            <h1>{pseudo}</h1>
             <button onClick={Logout}><ImExit aria-label='deconnexion' className='exit'/></button>
           </div>:<></>
           )}
