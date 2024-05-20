61a08fee51e9847eba03d8563708a0ba97a3e2ff
Hyrondelle <n.hyr@free.fr>
2024-05-20 11:25:41 +0200

    frinds style

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Friends = () => {
     return (
         <div className='friends'>
-            
+            <ul>
+                <li><h3>Victor Ramirez</h3><img src="src\assets\images4.jpeg" alt="prophil picture" /></li>
+                <li><h3>Isavelle Moreau</h3><img src="src\assets\images5.jpeg" alt="prophil picture" /></li>
+                <li><h3>Nicolas Heraut</h3><img src="src\assets\images6.jpeg" alt="prophil picture" /></li>
+                <li><h3>Vanessa Mirelt</h3><img src="src\assets\images7.jpeg" alt="prophil picture" /></li>
+            </ul>
         </div>
     );
 };
